eb3c4612499ff7d88a68c772d256d9661666bd8c
Robert Nees <robertnees@gmail.com>
2012-02-03 19:18:42 -0800

    Add new file to hold misc. dot sourced functions

---
+++ Do-Functions.ps1
@@ -0,0 +1,9 @@
+﻿## Copyright 2012 Robert Nees
+## Licensed under the Apache License, Version 2.0 (the "License");
+## http://sushihangover.blogspot.com
+##
+function isNumeric ($x) {
+    $x2 = 0
+    $isNum = [System.Int32]::TryParse($x, [ref]$x2)
+    return $isNum
+}
