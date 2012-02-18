9e905a6e4b302a6ab82e73c815489489339257ef
Robert Nees <robertnees@gmail.com>
2012-02-17 16:49:46 -0800

    Added new shutdown script to handle timed hibernates since shutdown does not allow a time to be added if using the /h option

---
+++ Do-Shutdown.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.NOTES
+Copyright 2012 Robert Nees
+Licensed under the Apache License, Version 2.0 (the "License");
+http://sushihangover.blogspot.com
+.SYNOPSIS
+System shutdown in X minutes
+.DESCRIPTION
+This script preforms a system shutdown or hibernate in X number of minutes
+.EXAMPLE
+Hibernate in 20 minutes:
+Do-Shutdown.ps1 20 -type h 
+.EXAMPLE
+Shutdown now:
+Do-Shutdown.ps1 0 -type s
+.LINK
+http://sushihangover.blogspot.com
+#>
+Param (
+        [parameter(
+            parametersetname="All",
+            mandatory=$true,
+            position=1)]
+            [Alias("min")]
+            [int]$minutes,
+        [parameter(
+            parametersetname="All",
+            mandatory=$false,
+            position=2)]
+            [string]$type = 'h'
+)
+write-host "Sleeping for" ($minutes) "minutes before shutdown (Ctrl-C to break)"
+start-sleep ($minutes * 60)
+shutdown /($type)
+Read-Host "Press ENTER to abort"
+shutdown /a
