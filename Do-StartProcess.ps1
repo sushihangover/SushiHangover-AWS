07c51d8042bbbaec2e1e395e5ddb8bef62308c47
Robert Nees <robertnees@gmail.com>
2012-02-25 08:33:24 -0800

    Changed this to a 'normal' function script vs being dot sourced

--- Do-StartProcess.ps1
+++ Do-StartProcess.ps1
@@ -1,29 +1,73 @@
-﻿## Copyright 2012 Robert Nees
-## Licensed under the Apache License, Version 2.0 (the "License");
-## http://sushihangover.blogspot.com
-##
+﻿<#
+.NOTES
+Copyright 2012 Robert Nees
+Licensed under the Apache License, Version 2.0 (the "License");
+http://sushihangover.blogspot.com
+.SYNOPSIS
+Start background process
+.DESCRIPTION
+This scripts provides a wrapper to System.Diagnostics.Process
+.EXAMPLE
+
+.LINK
+http://sushihangover.blogspot.com
+#>
+param(
+    [parameter(
+        parametersetname="All",
+        mandatory=$true,
+        position=1)]
+        [Alias("cmd")]      
+    [string]$cliCmd,
+    [parameter(
+        parametersetname="All",
+        mandatory=$false,
+        position=2)]
+        [Alias("args")]
+    [string]$cmdArgs="",
+    [parameter(
+        parametersetname="All",
+        mandatory=$false,
+        position=3)]
+        [Alias("wait")]
+    [boolean]$waitForExit = $true,
+    [parameter(
+        parametersetname="All",
+        mandatory=$false,
+        position=4)]
+        [Alias("priority")]
+    [string]$cmdPriority=[System.Diagnostics.ProcessPriorityClass]::Idle
+)
 function startProcess {
     [cmdletbinding(defaultparametersetname="All")]
     param(
         [parameter(
             parametersetname="All",
             mandatory=$true,
-            position=1)]      
+            position=1)]
+            [Alias("cmd")]      
         [string]$cliCmd,
         [parameter(
             parametersetname="All",
             mandatory=$false,
             position=2)]
+            [Alias("args")]
         [string]$cmdArgs="",
         [parameter(
             parametersetname="All",
             mandatory=$false,
             position=3)]
+            [Alias("wait")]
+        [boolean]$waitForExit = $true,
+        [parameter(
+            parametersetname="All",
+            mandatory=$false,
+            position=4)]
+            [Alias("priority")]
         [string]$cmdPriority=[System.Diagnostics.ProcessPriorityClass]::Idle
     )
-
     begin {
-        "parameterset: {0}; cliCmd: $cliCmd; cmdArgs: $cmdArgs; cmdPriority: $cmdPriority" -f $pscmdlet.parametersetname, $cliCmd, $cmdArgs, $cmdPriority
+#        "parameterset: {0}; cliCmd: $cliCmd; cmdArgs: $cmdArgs; cmdPriority: $cmdPriority" -f $pscmdlet.parametersetname, $cliCmd, $cmdArgs, $cmdPriority
     }
     end {
     	$ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo
@@ -32,6 +76,10 @@ function startProcess {
 	    $ProcessInfo.UseShellExecute = $False
 	    $newProcess = [System.Diagnostics.Process]::Start($ProcessInfo)
         $newProcess.PriorityClass = $cmdPriority
-        $newProcess.WaitForExit()
+        if ($waitForExit) {
+            $newProcess.WaitForExit()
+        }
     }
 }
+startProcess $cliCmd $cmdArgs $waitForExit $cmdPriority
+exit
