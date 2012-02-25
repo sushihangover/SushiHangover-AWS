b2108e045fd87655cfd03d360e77713a487e8cc1
Robert Nees <robertnees@gmail.com>
2012-02-25 10:33:35 -0800

    Kill process by name

---
+++ Do-KillByName.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.NOTES
+Copyright 2012 Robert Nees
+Licensed under the Apache License, Version 2.0 (the "License");
+http://sushihangover.blogspot.com
+.SYNOPSIS
+Kill process by name
+.DESCRIPTION
+This scripts provides a way to kill processes by name verus by Id
+.EXAMPLE
+Do-KillByName.ps1 bonjour
+.LINK
+http://sushihangover.blogspot.com
+#>
+$pName = $args[0]
+$killList = get-process | where {($_.Name -eq $pName)}
+If ($killList -ne $null) {
+    $killList
+    $killList | foreach { $_.Kill() }
+} Else {
+    echo "No processes found"
+}
