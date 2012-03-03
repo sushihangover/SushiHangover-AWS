5baefba0888b8349ad3e84c3942483779b022ff1
Robert Nees <robertnees@gmail.com>
2012-03-03 11:15:57 -0800

    Updated this script to handle pipelining the input and output, see examples in script...

--- Do-KillByName.ps1
+++ Do-KillByName.ps1
@@ -6,17 +6,43 @@ http://sushihangover.blogspot.com
 .SYNOPSIS
 Kill process by name
 .DESCRIPTION
-This scripts provides a way to kill processes by name verus by Id
+This scripts provides a way to kill processes by name vs. Id
+Do-KillByName.ps1 bonjour -whatif
 .EXAMPLE
-Do-KillByName.ps1 bonjour
+Do-KillByName.ps1 notepad
+.EXAMPLE
+Get-Process | Do-KillByName.ps1 -whatif | ft -AutoSize
+.EXAMPLE
+gwmi win32_process | where {$_.getowner().user -eq $env:username} | select handle | % { get-process -id $_.handle } | Do-killbyName -whatif
+Kill all the processes that the current user is running (this includes the powershell console that is running this oneliner!!)
 .LINK
 http://sushihangover.blogspot.com
 #>
-$pName = $args[0]
-$killList = get-process | where {($_.Name -eq $pName)}
-If ($killList -ne $null) {
-    $killList
-    $killList | foreach { $_.Kill() }
-} Else {
-    echo "No processes found"
+[CmdletBinding()] 
+Param(
+    [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$processName, 
+    [Parameter(Mandatory=$false)][switch]$whatif
+    )
+Begin {
+    $killList = @()
+}
+Process {
+    if ($_ -is [System.Diagnostics.Process]) {
+        $killMe = $_
+    } else {
+        $killMe = get-process | where {($_.Name -eq $processName)}
+    }
+    try {
+        if ($killMe -is [System.Diagnostics.Process]) {
+            $killList += $killMe
+            if (!$whatif.IsPresent) {
+                # $killMe.Kill()
+            }
+        }
+    } catch {
+        write-host "No process named $processName found" -ForegroundColor Red -BackgroundColor White
+    }
+}
+End {
+    return $killList
 }
