51536d9160ae33be893b1bbfc201104387a6879b
Robert Nees <robertnees@gmail.com>
2012-03-28 18:44:47 -0700

    Updated comments/help Removed script interface, dot source it and use it as a normal Function

--- Do-StartProcess.ps1
+++ Do-StartProcess.ps1
@@ -4,9 +4,19 @@ Copyright 2012 Robert Nees
 Licensed under the Apache License, Version 2.0 (the "License");
 http://sushihangover.blogspot.com
 .SYNOPSIS
-Start background process
+Start background process and return standard output
 .DESCRIPTION
-This scripts provides a wrapper to System.Diagnostics.Process
+This scripts provides a wrapper to System.Diagnostics.Process and Added stdout as the return 'object', it will be returned as
+an array of strings
+
+Priority Parameter Options:
+    Normal 	
+    Idle (default)
+    High
+    RealTime
+    BelowNormal
+    AboveNormal
+
 .EXAMPLE
 $ffprobe = "ffprobe.exe"
 $probeCMDLine = ' -prefix -show_streams -i "' + $file.FullName + '"'
@@ -14,82 +24,30 @@ $stdOut = Do-StartProcess.ps1 $ffprobe $probeCMDLine
 .LINK
 http://sushihangover.blogspot.com
 #>
-param(
-    [parameter(
-        parametersetname="All",
-        mandatory=$true,
-        position=1)]
-        [Alias("cmd")]      
-    [string]$cliCmd,
-    [parameter(
-        parametersetname="All",
-        mandatory=$false,
-        position=2)]
-        [Alias("args")]
-    [string]$cmdArgs="",
-    [parameter(
-        parametersetname="All",
-        mandatory=$false,
-        position=3)]
-        [Alias("wait")]
-    [boolean]$waitForExit = $true,
-    [parameter(
-        parametersetname="All",
-        mandatory=$false,
-        position=4)]
-        [Alias("priority")]
-    [string]$cmdPriority=[System.Diagnostics.ProcessPriorityClass]::Idle
-)
 function startProcess {
-    [cmdletbinding(defaultparametersetname="All")]
     param(
-        [parameter(
-            parametersetname="All",
-            mandatory=$true,
-            position=1)]
-            [Alias("cmd")]      
-        [string]$cliCmd,
-        [parameter(
-            parametersetname="All",
-            mandatory=$false,
-            position=2)]
-            [Alias("args")]
-        [string]$cmdArgs="",
-        [parameter(
-            parametersetname="All",
-            mandatory=$false,
-            position=3)]
-            [Alias("wait")]
-        [boolean]$waitForExit = $true,
-        [parameter(
-            parametersetname="All",
-            mandatory=$false,
-            position=4)]
-            [Alias("priority")]
-        [string]$cmdPriority=[System.Diagnostics.ProcessPriorityClass]::Idle
+        [parameter(parametersetname="All",mandatory=$true,position=1)][Alias("cmd")][string]$cliCmd,
+        [parameter(parametersetname="All",mandatory=$false,position=2)][Alias("args")][string]$cmdArgs="",
+        [parameter(parametersetname="All",mandatory=$false,position=3)][Alias("wait")][boolean]$waitForExit = $true,
+        [parameter(parametersetname="All",mandatory=$false,position=4)][Alias("priority")]
+            [string]$cmdPriority=[System.Diagnostics.ProcessPriorityClass]::Idle
     )
-    begin {
-#        "parameterset: {0}; cliCmd: $cliCmd; cmdArgs: $cmdArgs; cmdPriority: $cmdPriority" -f $pscmdlet.parametersetname, $cliCmd, $cmdArgs, $cmdPriority
-    }
-    end {
-    	$ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo
-    	$ProcessInfo.FileName = $cliCmd
-	    $ProcessInfo.Arguments = $cmdArgs
-	    $ProcessInfo.UseShellExecute = $False
-        $ProcessInfo.RedirectStandardInput = $false
-        $ProcessInfo.RedirectStandardOutput = $true
-	    $newProcess = [System.Diagnostics.Process]::Start($ProcessInfo)
-        $newProcess.PriorityClass = $cmdPriority
-        if ($waitForExit) {
-            $newProcess.WaitForExit()
-            $stdOut = @()
-            do {
-                $readLine = $newProcess.StandardOutput.ReadLine()
-                $stdOut += $readLine
-            } while ($readLine -ne $null)
+    $ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $ProcessInfo.FileName = $cliCmd
+	$ProcessInfo.Arguments = $cmdArgs
+	$ProcessInfo.UseShellExecute = $False
+    $ProcessInfo.RedirectStandardInput = $false
+    $ProcessInfo.RedirectStandardOutput = $true
+	$newProcess = [System.Diagnostics.Process]::Start($ProcessInfo)
+    $newProcess.PriorityClass = $cmdPriority
+    if ($waitForExit) {
+        $newProcess.WaitForExit()
+        $stdOut = @()
+        do {
+            $readLine = $newProcess.StandardOutput.ReadLine()
+            $stdOut += $readLine
+        } while ($readLine -ne $null)
 
-        }
-        return $stdout
     }
+    return $stdout
 }
-startProcess $cliCmd $cmdArgs $waitForExit $cmdPriority
