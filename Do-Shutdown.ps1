5e8b21427f42fb6fb73212dd00beaa22cbcaecb0
Robert Nees <robertnees@gmail.com>
2012-04-01 14:05:40 -0700

    Updated comments/notes

--- Do-Shutdown.ps1
+++ Do-Shutdown.ps1
@@ -1,32 +1,29 @@
 ﻿<#
 .NOTES
-Copyright 2012 Robert Nees
-Licensed under the Apache License, Version 2.0 (the "License");
-http://sushihangover.blogspot.com
+    Copyright 2012 Robert Nees
+    Licensed under the Apache License, Version 2.0 (the "License");
+    http://sushihangover.blogspot.com
 .SYNOPSIS
-Shutdown the computer with a Ctrl-C break
+    Shutdown the computer with a Ctrl-C break
 .DESCRIPTION
-This scripts provides a visual countdown bar till computer is shutdown/rebooted/hibernate and 
+    This scripts provides a visual countdown bar till computer is shutdown/rebooted/hibernate and 
 you can use Ctrl-C to break this countdown
 .EXAMPLE
-Do-Shutdown.ps1 30
+    Do-Shutdown 30
 .LINK
-http://sushihangover.blogspot.com
+    http://sushihangover.blogspot.com
 #>
 Param (
         [parameter(
-            parametersetname="All",
             mandatory=$true,
             position=1)]
             [Alias("min")]
             [int]$minutes,
         [parameter(
-            parametersetname="All",
             mandatory=$false,
             position=2)]
             [string]$type = 'h',
         [parameter(
-            parametersetname="All",
             mandatory=$false,
             position=3)]
             [switch]$whatif
